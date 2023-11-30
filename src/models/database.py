4203268aafc590d95ebb6a25fb0348ed355815ea
obafemi-i <obaff76@gmail.com>
2023-11-30 13:34:51 +0100

    models and database;

---
+++ src/models/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
+
+config = dotenv_values()
+
+DATABASE_URL = config['DATABASE_URL']
+
+engine = create_engine(DATABASE_URL)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ src/models/model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Integer, String, Column
+from .database import Base
+
+class User(Base):
+    __tablename__ = 'editors'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(20), unique=True, index=True)
+    password = Column(String(170))
+    first_name = Column(String(20), index=True)
