330b5ac86126c3caa8bcfa396ff9652c8bb7bc98
obafemi-i <obaff76@gmail.com>
2023-12-08 11:32:20 +0100

    added comments for google oauth function

--- src/utils/youtube_oauth.py
+++ src/utils/youtube_oauth.py
@@ -11,10 +11,13 @@ SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
 
 
 def authenticate_with_oauth():
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first time.
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json')
 
+    # If there are no (valid) credentials availabe, let the user log in
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -24,10 +27,11 @@ def authenticate_with_oauth():
             )
             creds = flow.run_local_server(port=0)
 
+        # Save credentials for the next run to avoid logging in every time
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
         
+        # Create and return Youtube API service object
         youtube = build('youtube', 'v3', credentials=creds)
         return youtube
     
-
